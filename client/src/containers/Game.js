e7b91ae4cc9327179be4638c9467a8ef76cb59ba
ronchen210402 <ronchen21@gmail.com>
2021-01-01 11:02:33 +0800

    client prototype done

---
+++ client/src/containers/Game.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Link, Redirect } from 'react-router-dom';
+
+export default function Game() {
+    return (
+        <>
+            <Redirect from="/" to="/login" />
+            <Route exact path="/login">
+                <Link to="/map">login</Link>
+            </Route>
+            <Route exact path="/map">
+                <div><Link to="/attack">attack</Link></div>
+                <div><Link to="/backpack">backpack</Link></div>
+            </Route>
+            <Route exact path="/attack">
+                <div>Attack not implement yet!!</div>
+                <div><Link to="/map">return</Link></div>
+            </Route>
+            <Route exact path="/backpack">
+                <div>Backpack not implement yet!!</div>
+                <div><Link to="/map">return</Link></div>
+            </Route>
+        </>
+    );
+}
