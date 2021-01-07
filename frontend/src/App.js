f3a6f35217efdf960357a434f83e729649fb9d15
tsukiyashirokisaki <55571376+tsukiyashirokisaki@users.noreply.github.com>
2021-01-07 14:37:13 +0800

    solve cor problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { clickToGet } from './axios' 
+import React ,{ useState } from 'react';
+
 function App() {
+  const [ischecked,setcheck] = useState(false);
+  const [msg,setmsg] = useState("");
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={async () => {
+          setmsg(await clickToGet())
+        }}
+      >
+      Click me!
+      </button>
+      <text>{msg}</text>
     </div>
   );
 }
