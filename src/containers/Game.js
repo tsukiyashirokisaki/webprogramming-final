69b38a221a1a5204bfa0ce98ac37a8e9b1fa5e66
tsukiyashirokisaki <55571376+tsukiyashirokisaki@users.noreply.github.com>
2021-01-08 13:12:56 +0800

    add grid

---
+++ src/containers/Game.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Link, Redirect } from 'react-router-dom';
+import MapView from './MapView';
+export default function Game() {
+    return (
+        <>
+            <Redirect from="/" to="/login" />
+            <Route exact path="/login">
+                <Link to="/map">login</Link>
+            </Route>
+            <Route exact path="/map">
+                <div><Link to="/attack">attack</Link></div>
+                <div><Link to="/backpack">backpack</Link></div>
+                <MapView username = "Ric" size = {40}></MapView>
+                
+            </Route>
+            <Route exact path="/attack">
+                <div>Attack not implement yet!!</div>
+                <div><Link to="/map">return</Link></div>
+            </Route>
+            <Route exact path="/backpack">
+                <div>Backpack not implement yet!!</div>
+                <div><Link to="/map">return</Link></div>
+            </Route>
+        </>
+    );
+}
