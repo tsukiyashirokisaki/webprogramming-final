546e104694df53a6b3234c8a17b84ef907e92372
ronchen210402 <ronchen21@gmail.com>
2021-01-19 17:03:20 +0800

    backpack done

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    backpack: [{ type: Schema.Types.ObjectId, ref: 'Pokemon' }]
 })
 
 const User = mongoose.model('User', UserSchema)
--- server/schema/Pokemon.js
+++ server/schema/Pokemon.js
@@ -21,45 +21,11 @@ const typeDefs = gql`
     extend type Query {
         findPokById(_id: ID!): Pokemon
     }
-
-    extend type Mutation {
-        addPok(pokIndex: Int!,
-               name: String!
-               nickname: String
-               cp: Int!
-               type: [String!]
-               skill: [String!]
-               evolution: [String!]
-               maxHp: Int!
-               baseATT: Int!
-               baseDEF: Int!
-               baseSTA: Int!): Pokemon
-    }
 `
 
 const resolvers = {
     Query: {
-        findPokById: async (parent, { _id }, context) => await Pokemon.find({ _id: _id })
-    },
-    Mutation: {
-        addPok: async (parent, {pokIndex, name, nickname, cp, type, skill, evolution, maxHp, baseATT, baseDEF, baseSTA }, context) => {
-            var data = new Pokemon({
-                pokIndex: pokIndex,
-                name: name,
-                nickname: nickname,
-                cp: cp,
-                type: type,
-                skill: skill,
-                evolution: evolution,
-                maxHp: maxHp,
-                hp: maxHp,
-                baseATT: baseATT,
-                baseDEF: baseDEF,
-                baseSTA: baseSTA
-            })
-
-            return await data.save()
-        }
+        findPokById: async (parent, { _id }, context) => await Pokemon.findOne({ _id: _id })
     }
 }
 
