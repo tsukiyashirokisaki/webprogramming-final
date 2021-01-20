7e975a51d6e1370216a6f0435eb09da392231b1a
Joshua Lau <josh.lau2011@gmail.com>
2021-01-21 05:43:11 +0800

    backpack interface (functionless)

--- client/src/containers/backpackView.js
+++ client/src/containers/backpackView.js
@@ -1,28 +1,51 @@
+import './backpackView.css'
+
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
-import LoginInputBox from '../components/loginInputBox';
-
 import { useQuery } from '@apollo/client'
-import { LogIn } from "../FetchData"
+import PokPreview from "../components/pokPreview"
 
 function BackpackView(props) {
-    // var backpack = []
-    // var idstring
+    var backpack = []
+    var idstring
 
-    // for (var i=0;i<props.backpack.length;i++){
-    //     idstring = props.backpack[i].pokIndex+""
-    //     while (idstring.length<3){
-    //         idstring = "0"+idstring
-    //     }
-    //     backpack.push({...props.backpack[i],img: require("./images/"+idstring+".png").default})
-    // }
-    // console.log(backpack)
+    for (var i=0;i<props.backpack.length;i++){
+        idstring = props.backpack[i].pokIndex+""
+        while (idstring.length<3){
+            idstring = "0"+idstring
+        }
+        backpack.push({...props.backpack[i],img: require("./images/"+idstring+".png").default})
+    }
+    console.log(backpack)
+
+    
 
     return (
         <>
             <div><Link to="/map">return</Link></div>
-            <div>Backpack not implement yet!!</div>
+            <div className="pokemon-list" style={{height: '100%', overflow: 'hidden'}}>
+                <nav>
+                <ul className="horizontal-list">
+                    {backpack.map((pok) => 
+                    <>
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    <PokPreview pokemon={pok} />
+                    </>
+                    )}
+                </ul>
+
+                </nav>
+            </div>
         </>
     )
 }
