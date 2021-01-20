af7d12d12ccc17f69f1752de1590123ec20542f5
ronchen210402 <ronchen21@gmail.com>
2021-01-21 04:19:40 +0800

    updeteCp Int -> Float

--- server/schema/Pokemon.js
+++ server/schema/Pokemon.js
@@ -47,7 +47,7 @@ const typeDefs = gql`
                baseSTA: Int!): Pokemon
         randomPop: Pokemon!
         updateHp(pokId:ID!, hp:Int!): Pokemon!
-        updateCp(pokId:ID!, cp:Int!): Pokemon!
+        updateCp(pokId:ID!, cp:Float!): Pokemon!
         evolution(pokId:ID!): Pokemon!
         deletePok(pokId:ID!): Boolean!
         curePok(pokId:ID!): Pokemon!
